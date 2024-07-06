29deda044961ec46b5b67311b335a1af94f930a5
Eevster <eevsterpokebedrock@gmail.com>
2024-07-06 12:07:54 -0500

    FirstCommit

--- obj/Debug/netcoreapp3.1/Pokemod.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235e323a051246892bae94ed79e559490cc209c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
