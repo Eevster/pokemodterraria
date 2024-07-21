3ceff56b615c8b12f7360231822943087267cf9d
JACSMITH215 <jacsmith215@gmail.com>
2024-07-21 18:12:08 -0500

    Pet Slot error fixed

--- Content/Pets/PokemonPetBuff.cs
+++ Content/Pets/PokemonPetBuff.cs
@@ -23,23 +23,27 @@ namespace Pokemod.Content.Pets
             if(GetType().Name.Contains("PetBuffShiny")) Shiny = true;
 
             if(player.miscEquips[0] != null && !player.miscEquips[0].IsAir){
-                CaughtPokemonItem pokeItem = (CaughtPokemonItem)player.miscEquips[0].ModItem;
-                if(pokeItem.proj == null || player.ownedProjectileCounts[ProjType] <= 0){
-                    if(pokeItem.PokemonName == PokeName && pokeItem.Shiny == Shiny){
-                        int proj = Projectile.NewProjectile(player.GetSource_Buff(buffIndex), player.Center, Vector2.Zero, ProjType, 0, 0f, player.whoAmI);
-                        pokeItem.proj = Main.projectile[proj];
-                    }else{
-                        player.ClearBuff(buffIndex);
+                if(player.miscEquips[0].ModItem is CaughtPokemonItem){
+                    CaughtPokemonItem pokeItem = (CaughtPokemonItem)player.miscEquips[0].ModItem;
+                    if(pokeItem.proj == null || player.ownedProjectileCounts[ProjType] <= 0){
+                        if(pokeItem.PokemonName == PokeName && pokeItem.Shiny == Shiny){
+                            int proj = Projectile.NewProjectile(player.GetSource_Buff(buffIndex), player.Center, Vector2.Zero, ProjType, 0, 0f, player.whoAmI);
+                            pokeItem.proj = Main.projectile[proj];
+                        }else{
+                            player.ClearBuff(buffIndex);
+                        }
                     }
                 }
             }
             if(player.ownedProjectileCounts[ProjType] > 0){
                 if(player.miscEquips[0] != null && !player.miscEquips[0].IsAir){
-                    CaughtPokemonItem pokeItem = (CaughtPokemonItem)player.miscEquips[0].ModItem;
-                    if(pokeItem.proj != null){
-                        if(pokeItem.PokemonName == PokeName && pokeItem.Shiny == Shiny){
-                            pokeItem.SetPetInfo();
-                            player.buffTime[buffIndex] = 10;
+                    if(player.miscEquips[0].ModItem is CaughtPokemonItem){
+                        CaughtPokemonItem pokeItem = (CaughtPokemonItem)player.miscEquips[0].ModItem;
+                        if(pokeItem.proj != null){
+                            if(pokeItem.PokemonName == PokeName && pokeItem.Shiny == Shiny){
+                                pokeItem.SetPetInfo();
+                                player.buffTime[buffIndex] = 10;
+                            }
                         }
                     }
                 }
