c52b2a5d138c41b1277126de226371913e50f3a3
Eevster <eevsterpokebedrock@gmail.com>
2024-08-17 14:34:23 -0500

    assembally updates

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7178df11909ebaf3410f72801e0965a923ecb043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0128f0b84050f332f84b4fad606414ba20854f09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
