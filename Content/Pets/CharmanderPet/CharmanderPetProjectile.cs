ca6a8d90faebfb46a9306114e117f9fe404f0051
Gage Priddy <43502651+CoffeeCanGaming@users.noreply.github.com>
2024-09-10 14:18:35 -0500

    Add files via upload

---
+++ Content/Pets/CharmanderPet/CharmanderPetProjectile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Pokemod.Content.Pets.CharmanderPet
+{
+    public class CharmanderPetProjectile : PokemonPetProjectile
+    {
+        public override int baseHP => 39;
+        public override int baseDef => 43;
+        public override int nAttackProjs => 3;
+        public override int baseDamage => 3;
+        public override int PokemonBuff => ModContent.BuffType<CharmanderPetBuff>();
+        public override float enemySearchDistance => 1000;
+        public override bool canAttackThroughWalls => false;
+        public override int attackDuration => 30;
+        public override int attackCooldown => 30;
+
+        public override int totalFrames => 20;
+        public override int animationSpeed => 5;
+        public override int[] idleStartEnd => [0, 6];
+        public override int[] walkStartEnd => [7, 13];
+        public override int[] jumpStartEnd => [8, 8];
+        public override int[] fallStartEnd => [10, 10];
+        public override int[] attackStartEnd => [14, 19];
+
+        public override string[] evolutions => ["Charmeleon"];
+        public override int levelToEvolve => 16;
+        public override int levelEvolutionsNumber => 1;
+
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.EyeOfCthulhuPet); // Copy the stats of the Suspicious Grinning Eye projectile
+
+            //Projectile.width = 36;
+
+            currentHp = 1000;
+
+            Projectile.width = 20;
+            DrawOffsetX = -(18 - Projectile.width / 2);
+            Projectile.height = 40;
+            Projectile.aiStyle = -1; // Use custom AI
+            Projectile.light = 1f;
+            Projectile.tileCollide = true;
+        }
+
+        public override void Attack(float distanceFromTarget, Vector2 targetCenter)
+        {
+            if (Projectile.owner == Main.myPlayer)
+            {
+                for (int i = 0; i < nAttackProjs; i++)
+                {
+                    if (attackProjs[i] == null)
+                    {
+                        attackProjs[i] = Main.projectile[Projectile.NewProjectile(Projectile.InheritSource(Projectile), Projectile.Center, 20f * Vector2.Normalize(targetCenter - Projectile.Center), ModContent.ProjectileType<Ember>(), GetPokemonDamage(), 2f, Projectile.owner)];
+                        currentStatus = (int)ProjStatus.Attack;
+                        SoundEngine.PlaySound(SoundID.Item34, Projectile.position);
+                        timer = attackDuration;
+                        canAttack = false;
+                        break;
+                    }
+                }
+            }
+        }
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            width = 20;
+            height = 32;
+            fallThrough = false;
+
+            return base.TileCollideStyle(ref width, ref height, ref fallThrough, ref hitboxCenterFrac);
+        }
+    }
+}
