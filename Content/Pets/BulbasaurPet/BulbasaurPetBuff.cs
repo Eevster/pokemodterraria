d801f8e3887165ef379c9b97203e2ee4aa2d8895
Gage Priddy <43502651+CoffeeCanGaming@users.noreply.github.com>
2024-09-10 14:17:05 -0500

    Add files via upload

---
+++ Content/Pets/BulbasaurPet/BulbasaurPetBuff.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Pokemod.Content.Pets.BulbasaurPet
+{
+	public class BulbasaurPetBuff : PokemonPetBuff
+	{
+        public override string PokeName => "Bulbasaur";
+        public override int ProjType => ModContent.ProjectileType<BulbasaurPetProjectile>();
+
+        public override void UpdateExtraChanges(Player player){
+            if (player.HasBuff(Type))
+            {
+                player.AddBuff(BuffID.Heartreach, 60); // Apply the first buff
+            }
+        }
+	}
+}
