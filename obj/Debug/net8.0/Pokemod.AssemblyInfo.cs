961e341fa4f9f5d6c11788962b75ef41381fa1ad
Eevster <eevsterpokebedrock@gmail.com>
2024-12-23 13:39:47 -0600

    Christmas Gastly and Magnemite Lines

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8482491cccfe3d761b33e6fb70c288cbd6d1f207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13173e3a4352a14144ddf98980efa5c5e717842")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
