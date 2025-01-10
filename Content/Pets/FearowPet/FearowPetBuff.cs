590c9ed5ca29dc58ab387cb1b036d1b24846db02
Eevster <eevsterpokebedrock@gmail.com>
2025-01-10 17:26:34 -0600

    Added base mons for Spearow and Fearow

---
+++ Content/Pets/FearowPet/FearowPetBuff.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Pokemod.Content.Pets.FearowPet
+{
+	public class FearowPetBuff: PokemonPetBuff
+	{
+        public override string PokeName => "Fearow";
+        public override int ProjType => ModContent.ProjectileType<FearowPetProjectile>();
+    }
+
+    public class FearowPetBuffShiny: PokemonPetBuff
+	{
+        public override string PokeName => "Fearow";
+        public override int ProjType => ModContent.ProjectileType<FearowPetProjectileShiny>();
+    }
+}
---
+++ Content/Pets/SpearowPet/SpearowPetBuff.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Pokemod.Content.Pets.SpearowPet
+{
+	public class SpearowPetBuff: PokemonPetBuff
+	{
+        public override string PokeName => "Spearow";
+        public override int ProjType => ModContent.ProjectileType<SpearowPetProjectile>();
+    }
+
+    public class SpearowPetBuffShiny: PokemonPetBuff
+	{
+        public override string PokeName => "Spearow";
+        public override int ProjType => ModContent.ProjectileType<SpearowPetProjectileShiny>();
+    }
+}
