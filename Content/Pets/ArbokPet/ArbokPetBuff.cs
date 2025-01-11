45f414c94d71f840462233d62879ceac49406099
Eevster <eevsterpokebedrock@gmail.com>
2025-01-11 15:51:15 -0600

    Arbok + Ekans base data

---
+++ Content/Pets/ArbokPet/ArbokPetBuff.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Pokemod.Content.Pets.ArbokPet
+{
+	public class ArbokPetBuff: PokemonPetBuff
+	{
+        public override string PokeName => "Arbok";
+        public override int ProjType => ModContent.ProjectileType<ArbokPetProjectile>();
+    }
+
+    public class ArbokPetBuffShiny: PokemonPetBuff
+	{
+        public override string PokeName => "Arbok";
+        public override int ProjType => ModContent.ProjectileType<ArbokPetProjectileShiny>();
+    }
+}
--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c451519b282917f73df83a2207bf728d8418da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+335640429935cc9ad3d7c10d5a044f15e7301131")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
