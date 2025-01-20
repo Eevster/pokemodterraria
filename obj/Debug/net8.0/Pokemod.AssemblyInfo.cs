51b5d099471759a15a3ce48e5f922ed8af654f5f
Eevster <eevsterpokebedrock@gmail.com>
2025-01-20 02:02:42 -0600

    Imported remaining beta mon files needed for kanto.

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b5a20a030cd2d43be6b4b82502d6337200a929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bde54d849a297c105eeba8b99e607527849e11d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
