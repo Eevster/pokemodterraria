d510428786f2f9ebe6e69410f235c5249f8b7e61
Eevster <eevsterpokebedrock@gmail.com>
2025-03-11 07:19:05 -0500

    Updated GengarCritter and GengarPet

--- Content/Pets/GengarPet/GengarPetProjectile.cs
+++ Content/Pets/GengarPet/GengarPetProjectile.cs
@@ -9,19 +9,32 @@ namespace Pokemod.Content.Pets.GengarPet
 {
 	public class GengarPetProjectile : PokemonPetProjectile
 	{
-		public override int hitboxWidth => 64;
-		public override int hitboxHeight => 64;
+		public override int hitboxWidth => 68;
+		public override int hitboxHeight => 72;
 
-		public override int totalFrames => 4;
+        public override int moveStyle => 2;
+
+        public override float distanceToFly => 300f;
+
+        public override float moveDistance1 => 800f;
+        public override float moveDistance2 => 500f;
+
+        public override int totalFrames => 12;
 		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
+		public override int[] idleStartEnd => [0,3];
+		public override int[] walkStartEnd => [4,7];
 		public override int[] jumpStartEnd => [0,3];
 		public override int[] fallStartEnd => [0,3];
 
-		
+        public override int[] attackStartEnd => [8, 11];
 
-		public override int nAttackProjs => 3;
+        public override int[] idleFlyStartEnd => [0, 3];
+        public override int[] walkFlyStartEnd => [0, 3];
+        public override int[] attackFlyStartEnd => [8, 11];
+
+
+
+        public override int nAttackProjs => 3;
 		public override float enemySearchDistance => 1000;
 		public override bool canAttackThroughWalls => true;
 		public override int attackDuration => 0;
