7ada41dc9bbe33a455145f79fc4012bd830abf54
Eevster <eevsterpokebedrock@gmail.com>
2025-06-19 00:02:16 -0500

    Dark ball Logic

---
+++ Content/Items/Pokeballs/DarkballItem.cs
@@ -0,0 +1,30 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Pokemod.Content.Items.Apricorns;
+using Terraria;
+
+namespace Pokemod.Content.Items.Pokeballs
+{
+	public class DarkballItem : BallItem
+	{
+		protected override int BallProj => ModContent.ProjectileType<DarkballProj>();
+		protected override int BallValue => 1000;
+		protected override float CatchRate => 1f;
+
+        public override void AddRecipes() {
+			CreateRecipe(8)
+				.AddIngredient(ModContent.ItemType<BlueApricorn>(), 1)
+				.AddRecipeGroup(RecipeGroupID.IronBar, 1)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+		}
+    }
+
+	public class DarkballProj : BallProj
+	{
+		public override void SetExtraPokemonEffects(ref CaughtPokemonItem pokeItem)
+		{
+			pokeItem.level = 100;
+		}
+	}
+}
--- obj/Release/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Release/net8.0/Pokemod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea6c23b395a26bd1ce68db4456ee4258e0dfa48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75306b1c3f1dd0d1cb07fc4c85f81e13d2eb11ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
