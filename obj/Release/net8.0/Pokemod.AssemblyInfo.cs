4a28f15f66255983662af153e92dfb5cbe537d96
Eevster <eevsterpokebedrock@gmail.com>
2025-07-05 01:30:33 -0500

    Let me sleep

--- obj/Release/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Release/net8.0/Pokemod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c62a585b071b05936d35bf7a8ca2a863f022cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcd5a1f3648966733de0d952fcdbcf20839a331")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
