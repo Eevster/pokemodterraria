a6210305ff787cfca645ee141a0aa3378f748288
Eevster <eevsterpokebedrock@gmail.com>
2025-07-15 11:14:17 -0500

    build.txt updated

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83594bfa6683466c71ff03a1e220fc06c15bd1ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd54a98dc97a6238ce2cc180d864c5eb7e20378f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
