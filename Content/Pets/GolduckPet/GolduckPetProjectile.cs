3088f3f4e85d38901949e323b8a794e690e68b2b
Fiona :3 <linuzspace@gmail.com>
2025-08-17 00:53:26 +0200

    Golduck and some fixes on psyduck

--- Content/Pets/GolduckPet/GolduckPetProjectile.cs
+++ Content/Pets/GolduckPet/GolduckPetProjectile.cs
@@ -12,12 +12,13 @@ namespace Pokemod.Content.Pets.GolduckPet
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int totalFrames => 18;
+		public override int animationSpeed => 6;
+		public override int[] idleStartEnd => [0, 7];
+		public override int[] walkStartEnd => [8, 16];
+		public override int[] jumpStartEnd => [10, 10];
+		public override int[] fallStartEnd => [13, 13];
+		public override int[] attackStartEnd => [17, 17];
 	}
 
 	public class GolduckPetProjectileShiny : GolduckPetProjectile{}
