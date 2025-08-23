fac7c10af6ecafee945693d2dcb4128a929d2918
Fiona :3 <linuzspace@gmail.com>
2025-08-23 12:47:59 +0200

    Pokemontitle update & Lapras + some sprite fixes
    Added detail to pokemontitle, added sprites for Lapras, and changed my old sprite

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3088f3f4e85d38901949e323b8a794e690e68b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2351cc88ed6b9db96448f2898181c3e5b9ba0d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
