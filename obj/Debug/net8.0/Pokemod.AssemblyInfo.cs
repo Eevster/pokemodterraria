de3d5687fe55d167de741d525de85f8a89fae509
Fiona :3 <linuzspace@gmail.com>
2025-08-24 23:39:43 +0200

    Exeggcute and Exeggutor
    Sprites for Exeggcute and Exeggutor

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2351cc88ed6b9db96448f2898181c3e5b9ba0d50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac7c10af6ecafee945693d2dcb4128a929d2918")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
