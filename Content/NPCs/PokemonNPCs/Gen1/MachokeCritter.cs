4c135e5b1b8ba183a4c0d4c2da81f3e5f45dc7d6
Fiona :3 <linuzspace@gmail.com>
2025-09-08 21:57:38 +0200

    Machop+Machoke+Machamp

--- Content/NPCs/PokemonNPCs/Gen1/MachokeCritter.cs
+++ Content/NPCs/PokemonNPCs/Gen1/MachokeCritter.cs
@@ -10,12 +10,12 @@ namespace Pokemod.Content.NPCs.PokemonNPCs
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int totalFrames => 11;
+		public override int animationSpeed => 10;
+		public override int[] idleStartEnd => [0,3];
+		public override int[] walkStartEnd => [4,9];
+		public override int[] jumpStartEnd => [4,7];
+		public override int[] fallStartEnd => [9,9];
 		public override float catchRate => 90;
 		public override int minLevel => 28;
 
--- Content/Pets/MachampPet/MachampPetProjectile.cs
+++ Content/Pets/MachampPet/MachampPetProjectile.cs
@@ -12,12 +12,13 @@ namespace Pokemod.Content.Pets.MachampPet
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int totalFrames => 11;
+		public override int animationSpeed => 8;
+		public override int[] idleStartEnd => [0, 3];
+		public override int[] walkStartEnd => [4, 7];
+		public override int[] jumpStartEnd => [5, 5];
+		public override int[] fallStartEnd => [7, 7];
+		public override int[] attackStartEnd => [8, 10];
 	}
 
 	public class MachampPetProjectileShiny : MachampPetProjectile{}
