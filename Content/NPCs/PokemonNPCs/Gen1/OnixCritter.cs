ee4bb3553ff762299617a6653be61eb241c4a682
Fiona :3 <linuzspace@gmail.com>
2025-09-25 22:14:54 +0200

    Onix and Tangela
    Added Onix and Tangela

--- Content/NPCs/PokemonNPCs/Gen1/OnixCritter.cs
+++ Content/NPCs/PokemonNPCs/Gen1/OnixCritter.cs
@@ -10,12 +10,12 @@ namespace Pokemod.Content.NPCs.PokemonNPCs
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int totalFrames => 15;
+		public override int animationSpeed => 6;
+		public override int[] idleStartEnd => [0, 7];
+		public override int[] walkStartEnd => [8, 13];
+		public override int[] jumpStartEnd => [10, 10];
+		public override int[] fallStartEnd => [6, 6];
 		public override float catchRate => 150;
 
 
--- Content/NPCs/PokemonNPCs/Gen1/TangelaCritter.cs
+++ Content/NPCs/PokemonNPCs/Gen1/TangelaCritter.cs
@@ -10,12 +10,12 @@ namespace Pokemod.Content.NPCs.PokemonNPCs
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int totalFrames => 12;
+		public override int animationSpeed => 8;
+		public override int[] idleStartEnd => [0,3];
+		public override int[] walkStartEnd => [4,9];
+		public override int[] jumpStartEnd => [4,4];
+		public override int[] fallStartEnd => [7,7];
 		public override float catchRate => 190;
 
 
