e090912654ee4bf409068e48c5381fa3318e11e2
JACSMITH215 <jacsmith215@gmail.com>
2026-01-04 13:31:51 -0500

    Spearow, Fearow

--- Content/NPCs/PokemonNPCs/Gen1/SpearowCritter.cs
+++ Content/NPCs/PokemonNPCs/Gen1/SpearowCritter.cs
@@ -7,26 +7,37 @@ namespace Pokemod.Content.NPCs.PokemonNPCs
 {
 	public class SpearowCritterNPC : PokemonWildNPC
 	{
-		public override int hitboxWidth => 28;
-		public override int hitboxHeight => 46;
+		public override int hitboxWidth => 20;
+		public override int hitboxHeight => 26;
 
-		public override int totalFrames => 4;
+		public override int totalFrames => 16;
 		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int moveStyle => 2;
+
+		public override int[] idleStartEnd => [0,3];
+		public override int[] walkStartEnd => [4,7];
+		public override int[] jumpStartEnd => [5,5];
+		public override int[] fallStartEnd => [6,6];
+		public override int[] attackStartEnd => [12,15];
+
+		public override int[] idleFlyStartEnd => [8,11];
+		public override int[] walkFlyStartEnd => [8,11];
+		public override int[] attackFlyStartEnd => [12,15];
 		public override float catchRate => 255;
+
+		public override int[][] spawnConditions =>
+		[
+			[(int)SpawnArea.Surface, (int)DayTimeStatus.All, (int)WeatherStatus.All]
+        ];
 
 		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) { 
 			base.SetBestiary(database, bestiaryEntry);
 			bestiaryEntry.AddTags(BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Surface);
 		}
+
 		public override float SpawnChance(NPCSpawnInfo spawnInfo) {
-			if (ModContent.GetInstance<BetaMonsConfig>().BetaMonsToggle) {
-				if (spawnInfo.Player.ZoneForest) {
-					return GetSpawnChance(spawnInfo, SpawnCondition.Overworld.Chance * 0.5f);
-			}
+			if (spawnInfo.Player.ZoneForest) {
+				return GetSpawnChance(spawnInfo, SpawnCondition.Overworld.Chance * 0.5f);
 			}
 
 			return 0f;
--- Content/Pets/FearowPet/FearowPetProjectile.cs
+++ Content/Pets/FearowPet/FearowPetProjectile.cs
@@ -9,15 +9,22 @@ namespace Pokemod.Content.Pets.FearowPet
 {
 	public class FearowPetProjectile : PokemonPetProjectile
 	{
-		public override int hitboxWidth => 28;
-		public override int hitboxHeight => 46;
+		public override int hitboxWidth => 48;
+		public override int hitboxHeight => 54;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int totalFrames => 16;
+		public override int animationSpeed => 6;
+		public override int moveStyle => 2;
+
+		public override int[] idleStartEnd => [0,3];
+		public override int[] walkStartEnd => [4,7];
+		public override int[] jumpStartEnd => [5,5];
+		public override int[] fallStartEnd => [6,6];
+		public override int[] attackStartEnd => [12,15];
+
+		public override int[] idleFlyStartEnd => [8,11];
+		public override int[] walkFlyStartEnd => [8,11];
+		public override int[] attackFlyStartEnd => [12,15];
 	}
 
 	public class FearowPetProjectileShiny : FearowPetProjectile{}
