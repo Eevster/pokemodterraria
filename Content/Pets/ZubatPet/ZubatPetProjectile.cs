6d2521777ae98e25d4b7779dd4d9b27f10f17594
Fiona :3 <linuzspace@gmail.com>
2026-01-11 23:57:30 +0100

    Zubat & Golbat
    Added Zubat and Golbat

--- Content/Pets/ZubatPet/ZubatPetProjectile.cs
+++ Content/Pets/ZubatPet/ZubatPetProjectile.cs
@@ -11,13 +11,17 @@ namespace Pokemod.Content.Pets.ZubatPet
 	{
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
+		public override int moveStyle => 1;
 
-		public override int totalFrames => 4;
+		public override int totalFrames => 6;
 		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+		public override int[] idleStartEnd => [0,2];
+		public override int[] walkStartEnd => [0,2];
+		
+		public override int[] idleFlyStartEnd => [0,2];
+		public override int[] walkFlyStartEnd => [0,2];
+		public override int[] attackFlyStartEnd => [3,5];
+		
 
 		public override string[] evolutions => ["Golbat"];
 		public override int levelToEvolve => 22;
