4e69398be9c4badac960e3388c692840e6ebab20
JACSMITH215 <jacsmith215@gmail.com>
2026-01-19 18:32:43 -0500

    Sudowoodo

---
+++ Content/NPCs/PokemonNPCs/Gen2/SudowoodoCritter.cs
@@ -0,0 +1,49 @@
+﻿using Terraria.GameContent.Bestiary;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace Pokemod.Content.NPCs.PokemonNPCs
+{
+	public class SudowoodoCritterNPC : PokemonWildNPC
+	{
+		public override int hitboxWidth => 28;
+		public override int hitboxHeight => 48;
+
+		public override int totalFrames => 22;
+		public override int animationSpeed => 6;
+		public override int[] idleStartEnd => [0, 15];
+		public override int[] walkStartEnd => [16, 21];
+		public override int[] jumpStartEnd => [16, 16];
+		public override int[] fallStartEnd => [17, 17];
+
+        public override float catchRate => 65;
+        public override int minLevel => 5;
+		
+		public override int[][] spawnConditions =>
+		[
+			[(int)SpawnArea.Jungle, (int)DayTimeStatus.All, (int)WeatherStatus.Clear],
+			[(int)SpawnArea.Surface, (int)DayTimeStatus.All, (int)WeatherStatus.Clear]
+        ];
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+		{
+			bestiaryEntry.AddTags(BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Jungle);
+            base.SetBestiary(database, bestiaryEntry);
+        }
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+			if (spawnInfo.Player.ZoneJungle)
+			{
+				return GetSpawnChance(spawnInfo, SpawnCondition.Overworld.Chance * 0.5f);
+			}
+			else if (spawnInfo.Player.ZoneForest)
+			{
+				return GetSpawnChance(spawnInfo, SpawnCondition.Overworld.Chance * 0.2f);
+			}
+
+			return 0f;
+		}
+	}
+
+	public class SudowoodoCritterNPCShiny : SudowoodoCritterNPC{}
+}
---
+++ Content/Pets/SudowoodoPet/SudowoodoPetProjectile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Pokemod.Content.Projectiles.PokemonAttackProjs;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Pokemod.Content.Pets.SudowoodoPet
+{
+	public class SudowoodoPetProjectile : PokemonPetProjectile
+	{
+		public override int hitboxWidth => 28;
+		public override int hitboxHeight => 48;
+
+		public override int totalFrames => 22;
+		public override int animationSpeed => 6;
+		public override int[] idleStartEnd => [0, 15];
+		public override int[] walkStartEnd => [16, 21];
+		public override int[] jumpStartEnd => [16, 16];
+		public override int[] fallStartEnd => [17, 17];
+	}
+
+	public class SudowoodoPetProjectileShiny : SudowoodoPetProjectile{}
+}
