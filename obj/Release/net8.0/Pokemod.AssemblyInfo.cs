9d27ba2e1e0a5c0b68f3c83958db1b5d8cd9f622
Eevster <eevsterpokebedrock@gmail.com>
2026-02-07 12:47:50 -0600

    Delibird Sprites

--- obj/Release/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Release/net8.0/Pokemod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2566475ffebadd4876b5e8e0ee3d027fa2b7bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092ac33bbc3742946fc9958b77971151de488847")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
