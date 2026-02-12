2edace5373272109beead0620a21def0f908edc6
RollinMan42 <joshnievesy@gmail.com>
2026-02-12 19:16:17 +0800

    Tentacool (Sprites by gotilies)

--- Content/Pets/TentacoolPet/TentacoolPetProjectile.cs
+++ Content/Pets/TentacoolPet/TentacoolPetProjectile.cs
@@ -12,14 +12,21 @@ namespace Pokemod.Content.Pets.TentacoolPet
 		public override int hitboxWidth => 28;
 		public override int hitboxHeight => 46;
 
-		public override int totalFrames => 4;
-		public override int animationSpeed => 5;
-		public override int[] idleStartEnd => [0,0];
-		public override int[] walkStartEnd => [0,3];
-		public override int[] jumpStartEnd => [0,3];
-		public override int[] fallStartEnd => [0,3];
+        public override int totalFrames => 22;
+        public override int animationSpeed => 6;
+        public override int[] idleStartEnd => [0, 5];
+        public override int[] walkStartEnd => [6, 13];
+        public override int[] jumpStartEnd => [9, 12];
+        public override int[] fallStartEnd => [4, 8];
+        public override int[] attackStartEnd => [14, 21];
 
-		public override string[] evolutions => ["Tentacruel"];
+        public override bool canSwim => true;
+
+        public override int[] idleSwimStartEnd => [0, 5];
+        public override int[] walkSwimStartEnd => [6, 13];
+        public override int[] attackSwimStartEnd => [14, 21];
+
+        public override string[] evolutions => ["Tentacruel"];
 		public override int levelToEvolve => 30;
 		public override int levelEvolutionsNumber => 1;
 	}
