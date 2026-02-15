16a53895e17e3dd010b1ee4bde56c7738a4be09c
Eevster <eevsterpokebedrock@gmail.com>
2026-02-15 15:29:39 -0600

    Mareep, Hoppip, Line Textures, and Vulpuix Textures

--- obj/Release/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Release/net8.0/Pokemod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304a7cc405fc6cd98dcd98a281fb78c03803926e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4739fdaf6a9eae50eecca35d05912305221ca672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
