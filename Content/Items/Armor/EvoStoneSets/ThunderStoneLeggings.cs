743e47495b34c19746a9af00c164d6cd95620ab0
RollinMan42 <joshnievesy@gmail.com>
2026-03-14 08:13:15 +0800

    Reorganising Armor files

---
+++ Content/Items/Armor/EvoStoneSets/ThunderStoneLeggings.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Pokemod.Content.Items.EvoStones;
+using Pokemod.Content.DamageClasses;
+
+namespace Pokemod.Content.Items.Armor.EvoStoneSets
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	// Providing the EquipType.Legs value here will result in TML expecting a X_Legs.png file to be placed next to the item's main texture.
+	[AutoloadEquip(EquipType.Legs)]
+	public class ThunderStoneLeggings : ModItem
+	{
+		public static readonly int MoveSpeedBonus = 5;
+		public static readonly int AdditiveGenericDamageBonus = 10;
+
+		public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(AdditiveGenericDamageBonus, AdditiveGenericDamageBonus / 2);
+
+		public override void SetDefaults() {
+			Item.width = 18; // Width of the item
+			Item.height = 18; // Height of the item
+			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+			Item.rare = ItemRarityID.Green; // The rarity of the item
+			Item.defense = 5; // The amount of defense the item will give when equipped
+		}
+
+		public override void UpdateEquip(Player player) {
+			player.GetDamage<PokemonDamageClass>() += AdditiveGenericDamageBonus / 100f;
+            player.GetDamage(DamageClass.Ranged) += AdditiveGenericDamageBonus / 200f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<ThunderStoneItem>(2)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
