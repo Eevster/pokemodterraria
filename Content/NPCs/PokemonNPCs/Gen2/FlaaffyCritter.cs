f012215afb6efcfffbef251f6d9ce300ccaa330b
RollinMan42 <joshnievesy@gmail.com>
2026-03-15 22:59:18 +0800

    Mareep line learnsets, artists mostly credited
    Added basic files for Flaaffy and Ampharos
    Edited scaling config recommendation tooltip
    Missed some artist credits I wasn't sure of

---
+++ Content/NPCs/PokemonNPCs/Gen2/FlaaffyCritter.cs
@@ -0,0 +1,43 @@
+﻿using Terraria.GameContent.Bestiary;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using Pokemod.Common.Configs;
+
+namespace Pokemod.Content.NPCs.PokemonNPCs
+{
+	public class FlaaffyCritterNPC : PokemonWildNPC
+	{
+		public override int hitboxWidth => 28;
+		public override int hitboxHeight => 46;
+
+		public override int totalFrames => 4;
+		public override int animationSpeed => 5;
+		public override int[] idleStartEnd => [0,0];
+		public override int[] walkStartEnd => [0,3];
+		public override int[] jumpStartEnd => [0,3];
+		public override int[] fallStartEnd => [0,3];
+        public override int[] attackStartEnd => [9, 9];
+        public override float catchRate => 120;
+        public override int minLevel => 15;
+
+        public override int[][] spawnConditions =>
+		[
+			[(int)SpawnArea.Surface, (int)DayTimeStatus.All, (int)WeatherStatus.All]
+		];
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) { 
+			base.SetBestiary(database, bestiaryEntry);
+			bestiaryEntry.AddTags(BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Surface);
+		}
+		public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+				if (spawnInfo.Player.ZoneForest) {
+					return GetSpawnChance(spawnInfo, SpawnCondition.Overworld.Chance * 0.2f);
+			}
+
+			return 0f;
+		}
+		
+	}
+
+	public class FlaaffyCritterNPCShiny : FlaaffyCritterNPC { }
+}
---
+++ Content/Pets/AmpharosPet/AmpharosPetProjectile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Pokemod.Content.Projectiles.PokemonAttackProjs;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Pokemod.Content.Pets.DoduoPet
+{
+	public class AmpharosPetProjectile : PokemonPetProjectile
+	{
+		public override int hitboxWidth => 28;
+		public override int hitboxHeight => 46;
+
+		public override int totalFrames => 4;
+		public override int animationSpeed => 5;
+		public override int[] idleStartEnd => [0,0];
+		public override int[] walkStartEnd => [0,3];
+		public override int[] jumpStartEnd => [0,3];
+		public override int[] fallStartEnd => [0,3];
+	}
+
+	public class AmpharosPetProjectileShiny : AmpharosPetProjectile { }
+}
