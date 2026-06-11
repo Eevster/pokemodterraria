841305021670372ad108661e96053ca3ad9ba2d4
Fiona :3 <linuzspace@gmail.com>
2026-06-11 10:32:22 +0200

    Onix animation change
    Onix animation change

--- Content/Pets/OnixPet/OnixPetProjectile.cs
+++ Content/Pets/OnixPet/OnixPetProjectile.cs
@@ -12,13 +12,13 @@ namespace Pokemod.Content.Pets.OnixPet
 		public override int hitboxWidth => 92;
 		public override int hitboxHeight => 78;
 
-		public override int totalFrames => 11;
-		public override int animationSpeed => 10;
-		public override int[] idleStartEnd => [0, 3];
-		public override int[] walkStartEnd => [4, 9];
+		public override int totalFrames => 5;
+		public override int animationSpeed => 15;
+		public override int[] idleStartEnd => [0, 1];
+		public override int[] walkStartEnd => [0, 3];
 		public override int[] jumpStartEnd => [0, 0];
 		public override int[] fallStartEnd => [1, 1];
-		public override int[] attackStartEnd => [10, 10];
+		public override int[] attackStartEnd => [4, 4];
 	}
 
 	public class OnixPetProjectileShiny : OnixPetProjectile{}
