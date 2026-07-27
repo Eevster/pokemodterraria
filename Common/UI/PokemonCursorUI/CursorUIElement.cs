9f4318d7c7eec995ca46a8b154a82777cf415924
JACSMITH215 <jacsmith215@gmail.com>
2026-07-27 05:24:35 -0500

    Pokmon cursors fix

--- Common/UI/PokemonCursorUI/CursorUIElement.cs
+++ Common/UI/PokemonCursorUI/CursorUIElement.cs
@@ -46,7 +46,7 @@ namespace Pokemod.Common.UI.PokemonCursorUI
             // Draw an arrow for each pokemon
             foreach (int index in Main.player[Main.myPlayer].GetModPlayer<PokemonPlayer>().currentActivePokemon)
             {
-                if (Main.projectile[index].active)
+                if (Main.projectile[index] != null && Main.projectile[index].active)
                 {
                     PokemonPetProjectile PokemonProj;
 
@@ -60,43 +60,43 @@ namespace Pokemod.Common.UI.PokemonCursorUI
                         {
                             continue;
                         }
+                    
+                        var pokemonVector = PokemonProj.Projectile.Center - playerPos;
+                        
+                        // reverse arrow if gravitation potion effect is active
+                        pokemonVector.Y *= Main.LocalPlayer.gravDir;
+                        
+                        // Defines variables used to for drawing
+                        var modifier = Math.Clamp(1.15f - 1 / (2f * Main.screenWidth) * pokemonVector.Length(), 0.02f, 1f);
+                        var alpha = modifier * 0.9f;
+                        var scale = modifier * 1.2f ;
+                        pokemonVector.Normalize();
+                        var arrowPos = playerPos + pokemonVector * ArrowDistance - Main.screenPosition;
+                        arrowPos *= posScaleFactor;
+
+                        var rotation = (float) Math.Atan2(pokemonVector.Y, pokemonVector.X);
+                        
+                        // Draw the arrow
+                        var tex = ModContent.Request<Texture2D>(CursorTexturePath).Value;
+                        spriteBatch.Draw(tex, arrowPos, null, PokemonProj.GetHPBarColor() * alpha, rotation, tex.Size() / 2f, 1.2f * ArrowScale, SpriteEffects.None, 1);
+                        var texBorder = ModContent.Request<Texture2D>(CursorTexturePath + "_Border").Value;
+                        spriteBatch.Draw(texBorder, arrowPos, null, Color.White * alpha, rotation, tex.Size() / 2f, 1.2f * ArrowScale, SpriteEffects.None, 1);
+
+                        // Draw the pokemon head
+                        var headTex = ModContent.Request<Texture2D>("Pokemod/Assets/Textures/Pokesprites/Icons/"+PokemonProj.Name.Replace("PetProjectile","")).Value;
+                        var headPos = playerPos + pokemonVector * (ArrowDistance - (PokemonImageDistance * Main.UIScale) * ArrowScale) - Main.screenPosition;
+                        headPos *= posScaleFactor;
+                        spriteBatch.Draw(
+                            headTex,
+                            headPos,
+                            null,
+                            Color.White * alpha,
+                            0f,
+                            headTex.Size() * 0.5f,
+                            scale * ArrowScale,
+                            SpriteEffects.None,
+                            0);
                     }
-
-                    var pokemonVector = PokemonProj.Projectile.Center - playerPos;
-                    
-                    // reverse arrow if gravitation potion effect is active
-                    pokemonVector.Y *= Main.LocalPlayer.gravDir;
-                    
-                    // Defines variables used to for drawing
-                    var modifier = Math.Clamp(1.15f - 1 / (2f * Main.screenWidth) * pokemonVector.Length(), 0.02f, 1f);
-                    var alpha = modifier * 0.9f;
-                    var scale = modifier * 1.2f ;
-                    pokemonVector.Normalize();
-                    var arrowPos = playerPos + pokemonVector * ArrowDistance - Main.screenPosition;
-                    arrowPos *= posScaleFactor;
-
-                    var rotation = (float) Math.Atan2(pokemonVector.Y, pokemonVector.X);
-                    
-                    // Draw the arrow
-                    var tex = ModContent.Request<Texture2D>(CursorTexturePath).Value;
-                    spriteBatch.Draw(tex, arrowPos, null, PokemonProj.GetHPBarColor() * alpha, rotation, tex.Size() / 2f, 1.2f * ArrowScale, SpriteEffects.None, 1);
-                    var texBorder = ModContent.Request<Texture2D>(CursorTexturePath + "_Border").Value;
-                    spriteBatch.Draw(texBorder, arrowPos, null, Color.White * alpha, rotation, tex.Size() / 2f, 1.2f * ArrowScale, SpriteEffects.None, 1);
-
-                    // Draw the pokemon head
-                    var headTex = ModContent.Request<Texture2D>("Pokemod/Assets/Textures/Pokesprites/Icons/"+PokemonProj.Name.Replace("PetProjectile","")).Value;
-                    var headPos = playerPos + pokemonVector * (ArrowDistance - (PokemonImageDistance * Main.UIScale) * ArrowScale) - Main.screenPosition;
-                    headPos *= posScaleFactor;
-                    spriteBatch.Draw(
-                        headTex,
-                        headPos,
-                        null,
-                        Color.White * alpha,
-                        0f,
-                        headTex.Size() * 0.5f,
-                        scale * ArrowScale,
-                        SpriteEffects.None,
-                        0);
                 }
             }
         }
