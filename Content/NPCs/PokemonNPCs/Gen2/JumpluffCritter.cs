7541c0028121da063d4af728dcd293b844e33f66
JACSMITH215 <jacsmith215@gmail.com>
2026-08-11 14:13:04 -0500

    Jumpluff spritesheet fixed this time for real

--- Content/NPCs/PokemonNPCs/Gen2/JumpluffCritter.cs
+++ Content/NPCs/PokemonNPCs/Gen2/JumpluffCritter.cs
@@ -12,19 +12,19 @@ namespace Pokemod.Content.NPCs.PokemonNPCs
         public override int hitboxWidth => 64;
         public override int hitboxHeight => 60;
 
-        public override int totalFrames => 59;
+        public override int totalFrames => 58;
         public override int animationSpeed => 5;
         public override int moveStyle => 2;
 
-        public override int[] idleStartEnd => [20, 29];
-        public override int[] walkStartEnd => [41, 58];
-        public override int[] jumpStartEnd => [29, 34];
+        public override int[] idleStartEnd => [20, 28];
+        public override int[] walkStartEnd => [41, 57];
+        public override int[] jumpStartEnd => [29, 33];
         public override int[] fallStartEnd => [10, 19];
-        public override int[] attackStartEnd => [34, 41];
+        public override int[] attackStartEnd => [34, 40];
 
-        public override int[] idleFlyStartEnd => [10, 19];
-        public override int[] walkFlyStartEnd => [10, 19];
-        public override int[] attackFlyStartEnd => [0, 10];
+        public override int[] idleFlyStartEnd => [10,19];
+		public override int[] walkFlyStartEnd => [10,19];
+		public override int[] attackFlyStartEnd => [0, 9];
 
         public override float catchRate => 180;
 
