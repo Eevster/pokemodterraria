b4bd047179dcfba5b3bae2ff2513bed862e19094
Eevster <eevsterpokebedrock@gmail.com>
2026-09-19 14:47:03 -0500

    Fixed Litwick Line Icon Alignment

--- obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pokemod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955c2ace57282f9d04ec8ab57e5f3302418c4713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097fd12030ad46957ecca05529563819c95828d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
